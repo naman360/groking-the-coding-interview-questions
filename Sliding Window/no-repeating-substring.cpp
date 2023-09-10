358a553037626bb742be3472dbcad1f4d6d7d7c9
Naman Shankhydhar <naman.geek@gmail.com>
2023-09-10 17:14:07 +0530

    no-repeating-substring other approach

--- Sliding Window/no-repeating-substring.cpp
+++ Sliding Window/no-repeating-substring.cpp
@@ -16,24 +16,34 @@ int noRepeatingSubstring(string s)
     int maxi = -1e9;
     int j = 0;
     unordered_map<char, int> mp;
-    for (int i = 0; i < s.size(); i++) // abccde
-    {
-        mp[s[i]]++;
+    // iterate and when encountered a repeating character shrink the window until there is no repeating char
+    // for (int i = 0; i < s.size(); i++)
+    // {
+    //     mp[s[i]]++;
 
-        while (mp[s[i]] > 1)
+    //     while (mp[s[i]] > 1)
+    //     {
+    //         mp[s[j]]--;
+    //         if (mp[s[j]] == 0)
+    //             mp.erase(s[j]);
+    //         j++;
+    //     }
+    //     maxi = max(i - j + 1, maxi);
+    // }
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (mp.find(s[i]) != mp.end())
         {
-            mp[s[j]]--;
-            if (mp[s[j]] == 0)
-                mp.erase(s[j]);
-            j++;
+            j = max(j, mp[s[i]] + 1); // shrink window till the next element of last occurence of repeating char (or the next repeating character)
         }
+        mp[s[i]] = i; // store the last index at which the character has beenencountered
         maxi = max(i - j + 1, maxi);
     }
     return maxi;
 }
 int main()
 {
-    string s = "aabccbb";
+    string s = "abccde";
     cout << noRepeatingSubstring(s);
     return 0;
 }
