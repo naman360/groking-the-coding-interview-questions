39e501b78a8582dccb045a4d99f0f9034c754df9
Naman Shankhydhar <naman.geek@gmail.com>
2023-09-20 10:04:42 +0530

    triplet sum to 0

--- Two Pointers/triplet-sum-to-zero.cpp
+++ Two Pointers/triplet-sum-to-zero.cpp
@@ -12,42 +12,52 @@ using namespace std;
 
 vector<vector<int>> triplet(vector<int> v)
 {
+    // cout << "Hello";
 
     int i = 0, low, high;
     sort(v.begin(), v.end());
-
     // -3 -2 -1 -1 0 1 2
     vector<vector<int>> ans;
-    while (i <= v.size() - 3)
+    while (i < v.size())
     {
-        int tofind = 0 - v[i];
-        low = i + 1, high = v.size() - 1;
-        while (low < high)
+        if (i != 0 && v[i] == v[i - 1])
+            i++;
+        else
         {
-            if (v[low] + v[high] == tofind)
+
+            low = i + 1, high = v.size() - 1;
+            while (low < high)
             {
-                vector<int> p;
-                p.push_back(v[i]);
-                p.push_back(v[low]);
-                p.push_back(v[high]);
-                ans.push_back(p);
+                int sum = v[i] + v[low] + v[high];
+                if (sum == 0)
+                {
+                    vector<int> p = {v[i], v[low], v[high]};
+                    ans.push_back(p);
+                    low++;
+                    high--;
+                    while (low < high && v[low] == v[low - 1])
+                        low++;
+                    while (low < high && v[high] == v[high + 1])
+                        high--;
+                }
+                else if (sum > 0)
+                {
+                    high--;
+                }
+                else
+                    low++;
             }
-            else if (v[low] + v[high] > tofind)
-            {
-                high--;
-            }
-            else
-                low++;
+            i++;
         }
-        i++;
     }
+
     return ans;
 }
 
 int main()
 {
 
-    vector<int> v = {-3, 1, 2, 4};
+    vector<int> v{-3, 0, 1, 2, -1, 1, -2};
     vector<vector<int>> ans = triplet(v);
     for (auto v : ans)
     {
